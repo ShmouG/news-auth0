863e46d50902d465d110d8170c9f29fab6502d16
Sam Gross <sammiel.gross@gmail.com>
2019-03-01 19:58:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import News from './News';
+import Sidenews from './Sidenews';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news1: {
+        type: 'top-headlines',
+        query: 'sources=google-news'
+      },
+      news2: {
+        type: 'everything',
+        query: 'sources=the-guardian-uk'
+      },
+      news3: {
+        type: 'everything',
+        query: 'sources=reddit-r-all'
+      }
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid">
+        <div className="navbar-fixed">
+          <nav>
+            <div className="nav-wrapper indigo lighten-4">
+              <a href="/" className="bran-logo center">My Feed</a>
+            </div>
+          </nav>
+        </div>
+        <div className="row">
+          <div className="col s8">
+            <News news={this.state.news1} />
+            <News news={this.state.news2} />
+          </div>
+          <div className="col s4">
+            <Sidenews news={this.state.news3}/>
+          </div>
+        </div>
       </div>
     );
   }
