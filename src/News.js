a9b90619845404b0d18f90207a042fa649ae9245
Sam Gross <sammiel.gross@gmail.com>
2019-05-09 13:16:42 -0500

    npm update

--- src/News.js
+++ src/News.js
@@ -1,54 +1,52 @@
 import React, { Component } from 'react';
 import NewSingle from './NewSingle';
-import Error from './Error'
+import Error from './Error';
 
 class News extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            news: [],
-            error: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+      error: false,
+    };
+  }
 
-        };
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=ae3aff618e7d4b71b666bd9d0b10f053`;
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error: true
+        })
+      });
+  }
+
+  renderItems() {
+    if (!this.state.error) {
+      return this.state.news.map((item) => (
+        <NewSingle key={item.url} item={item} />
+      ));
+    } else {
+      return <Error />
     }
+  }
 
-    componentDidMount() {
-        const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=ae3aff618e7d4b71b666bd9d0b10f053`;
-
-        fetch(url)
-         .then((response) => {
-             return response.json();
-         })
-         .then((data) => {
-             this.setState({
-                 news: data.articles
-             })
-         })
-         .catch((error) => {
-            this.setState({
-                error: true
-            })
-        });
-        
-    }
-
-    renderItems() {
-        if (!this.state.error) {
-            return this.state.news.map(function (item) {
-              return  <NewSingle key={item.url} item={item} />
-            });
-        } else {
-            return <Error />
-        }
-    }
-
-    render() {
-        return (
-            <div className='row'>
-                {this.renderItems()}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="row">
+        {this.renderItems()}
+      </div>
+    );
+  }
 }
 
 export default News;
